5233f2566b1e74483d42551d0f13ce2d6f9d66b8
ebrillhart <emma.brillhart@gmail.com>
2015-12-08 16:09:46 -0800

    actually fixed

--- public/app/services.js
+++ public/app/services.js
@@ -1,5 +1,7 @@
-angular.module('DrinkServices', ['ngResource']).factory('Drink', ['$resource', "$routeParams", function($resource, $routeParams) {
+angular.module('DrinkServices', ['ngResource']).factory('Ingredients', ['$resource', "$routeParams", function($resource, $routeParams) {
     return $resource('http://localhost:3000/api/drinks/:id/ingredients', {}, {
-    	saveIngredients: {method: 'POST'}
+    	saveIngredients: {method: 'POST'},
     });
+}]).factory('Drink', ['$resource', "$routeParams", function($resource, $routeParams) {
+    return $resource('http://localhost:3000/api/drinks/:id');
 }]);
