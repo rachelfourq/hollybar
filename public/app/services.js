a734a101f9a5bb894ade76bea2508516cf74addc
Brittany Brassell <brittany.brassell@gmail.com>
2015-12-08 18:50:33 -0800

    adjusting for working show page

--- public/app/services.js
+++ public/app/services.js
@@ -1,7 +1,10 @@
-angular.module('DrinkServices', ['ngResource']).factory('Ingredients', ['$resource', "$routeParams", function($resource, $routeParams) {
+angular.module('DrinkServices', ['ngResource'])
+	.factory('Ingredients', ['$resource', "$routeParams", 
+		function($resource, $routeParams) {
     return $resource('/api/drinks/:id/ingredients', {}, {
     	saveIngredients: {method: 'POST'}
     });
-}]).factory('Drink', ['$resource', "$routeParams", function($resource, $routeParams) {
-    return $resource('/api/drinks/:id');
+}]).factory('Drink', ['$resource', "$routeParams", 
+		function($resource, $routeParams) {
+    return $resource('api/drinks/:id');
 }]);
